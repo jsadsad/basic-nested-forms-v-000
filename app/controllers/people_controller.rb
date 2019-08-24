9324aa7f2756e9e603049026398335ab94a742fd
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-24 18:15:53 +0000

    Automatically backed up by Learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -17,6 +17,14 @@ class PeopleController < ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(
+    :name,
+    :street_address_1,
+    :street_address_2,
+    :city,
+    :state,
+    :zipcode,
+    :address_type
+    )
   end
 end
